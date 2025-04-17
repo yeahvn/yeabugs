22eeaab35805febd4a5dc69c85b9bb3ad5c86199
yeahvn <yeeeeeabug@naver.com>
2025-04-17 10:44:09 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+from time import sleep
+
+
+st.set_page_config(
+    page_icon="🌍",
+    page_title="인공지능 -- 배포",
+    layout="wide"
+)
+
+with st.spinner(text="페이지 로딩중 . . ."):
+    sleep(3)
+
+st.header("페이지에 오신걸 환영합니다.")
+st.subheader("기능 맛보기")
+
+cols = st.columns((1, 1, 2))
+cols[0].metric("10/11", "15 °C", "2")
+cols[0].metric("10/12", "17 °C", "2 °F")
+cols[0].metric("10/13", "15 °C", "2")
+cols[1].metric("10/14", "17 °C", "2 °F")
+cols[1].metric("10/15", "14 °C", "-3 °F")
+cols[1].metric("10/16", "13 °C", "-1 °F")
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+
+cols[2].line_chart(chart_data)
